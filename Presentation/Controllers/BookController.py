e6da760ceafb801fb719d0270a30a00d3a05326f
Deepsky Skynet <victorjhampier@gmail.com>
2022-10-07 00:48:37 -0500

    Correct some libraries book controller

--- Presentation/Controllers/BookController.py
+++ Presentation/Controllers/BookController.py
@@ -6,23 +6,8 @@ from Domain.Entities.Data.CopyDataEntity import CopyDataEntity
 from Domain.Entities.Data.ItemDataEntity import ItemDataEntity
 from Domain.Entities.Data.PublisherDataEntity import PublisherDataEntity
 from Domain.Entities.Data.SerialTitlesDataEntity import SerialTitlesDataEntity
-from Domain.Entities.Response import Response
-from datetime import datetime
-
-from Domain.Interfaces.IAuthor import IAuthor
-from Domain.Interfaces.IClassification import IClassification
-from Domain.Interfaces.ICopy import ICopy
 from Domain.Interfaces.IEasyResponse import IEasyResponse
-from Domain.Interfaces.IItem import IItem
-from Domain.Interfaces.IPublisher import IPublisher
-from Domain.Interfaces.ISerialTitle import ISerialTitle
-from Infrastructure.Neo4j.Nodes.AuthorNode import AuthorNode
 from Domain.Common.EasyResponseCommon import EasyResponseCommon
-from Infrastructure.Neo4j.Nodes.ClassificationNode import ClassificationNode
-from Infrastructure.Neo4j.Nodes.CopyNode import CopyNode
-from Infrastructure.Neo4j.Nodes.ItemNode import ItemNode
-from Infrastructure.Neo4j.Nodes.PublisherNode import PublisherNode
-from Infrastructure.Neo4j.Nodes.SerialTitleNode import SerialTitleNode
 
 EasyResponse:IEasyResponse = EasyResponseCommon()
 
